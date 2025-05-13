283364b0a870a0f3bb5a4a70c4487f21414473e1
Alexander Schindler <a7exschin@proton.me>
2025-05-13 21:17:12 +0200

    Add latest

---
+++ src/lib/components/sponsor_banner/Banner.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { getContext } from 'svelte';
+</script>
+
+<div class="px-3">
+	<div class="text-center dark:text-white text-xl font-semibold z-50">
+	    {$i18n.t('Sponsor')}
+	</div>
+</div>
