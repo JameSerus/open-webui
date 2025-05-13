c9992269ce63ad2f43dfd40c18a8c12bd8e85712
Alexander Schindler <a7exschin@proton.me>
2025-05-13 21:28:52 +0200

    Rename module

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Chat from '$lib/components/chat/Chat.svelte';
-	import Banner from '$lib/components/sponsor_banner/Banner.svelte';
+	import Banner from '$lib/components/legal_disclaimer/Banner.svelte';
 </script>
 
 <Chat />
