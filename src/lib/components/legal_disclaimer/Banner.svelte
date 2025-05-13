b0a1630c4c175a37ffa2bd11746cfb15b620212d
Alexander Schindler <a7exschin@proton.me>
2025-05-13 22:56:29 +0200

    Add Disclaimer for Legal Notice

--- src/lib/components/legal_disclaimer/Banner.svelte
+++ src/lib/components/legal_disclaimer/Banner.svelte
@@ -1,9 +1,11 @@
 <script>
 	import { getContext } from 'svelte';
+
+	const i18n = getContext('i18n');
 </script>
 
 <div class="px-3">
-	<div class="text-center dark:text-white text-xl font-semibold z-50">
-	    {$i18n.t('Sponsor')}
+	<div class="text-center font-semibold z-50">
+	    {$i18n.t('Disclaimer')}
 	</div>
 </div>
