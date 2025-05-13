8c1a43ef477f664c1f32fea49ddea09f63d54527
Alexander Schindler <a7exschin@proton.me>
2025-05-13 21:20:57 +0200

    Add Banner to page svelte

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Chat from '$lib/components/chat/Chat.svelte';
 	import Help from '$lib/components/layout/Help.svelte';
+	import Banner from '$lib/components/sponsor_banner/Banner.svelte';
 </script>
 
 <Help />
 <Chat />
+<Banner />
